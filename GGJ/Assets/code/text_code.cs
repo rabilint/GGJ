679fdb654233340b2332574a2b27c39c6e37b9e8
rabilint <rabilint2007@gmail.com>
2023-02-07 18:21:17 +0200

    x

---
+++ GGJ/Assets/code/text_code.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class text_code : MonoBehaviour
+{
+    public TMPro.TMP_Text "text" ;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
